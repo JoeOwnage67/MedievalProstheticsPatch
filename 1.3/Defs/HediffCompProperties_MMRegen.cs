a431d14c8f213b8c13d4981c8e419ecf7314a545
JoeOwnage67 <110196881+JoeOwnage67@users.noreply.github.com>
2022-09-21 09:28:59 -0400

    Create HediffCompProperties_MMRegen.cs

---
+++ 1.3/Defs/HediffCompProperties_MMRegen.cs
@@ -0,0 +1,23 @@
+using Verse;
+
+namespace MM
+{
+    // Token: 0x02000019 RID: 25
+    public class HediffCompProperties_MMRegen : HediffCompProperties
+    {
+        // Token: 0x04000044 RID: 68
+        public int RegenHealVal;
+
+        // Token: 0x04000043 RID: 67
+        public int RegenHoursMax;
+
+        // Token: 0x04000042 RID: 66
+        public int RegenHoursMin;
+
+        // Token: 0x06000048 RID: 72 RVA: 0x00003E26 File Offset: 0x00002026
+        public HediffCompProperties_MMRegen()
+        {
+            compClass = typeof(HediffComp_MMRegen);
+        }
+    }
+}
